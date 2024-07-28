afc1b64b2613501c183e51dbf0087afa1a269fa6
Chandra307 <chandra19151425@gmail.com>
2024-07-28 06:08:14 +0530

    SSRendering

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const userRouter = require('./src/routes/user');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
@@ -8,6 +9,16 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/user', userRouter);
+
+app.use((req, res, next) => {
+    let fileName;
+    
+    if (req.url === '/' || req.url === '/login.html') fileName = '/login.html'
+    else fileName = req.url;
+    
+    let filePath =  path.join(__dirname, '../', '/frontend', fileName);
+    res.sendFile(filePath);
+});
 
 const PORT = process.env.PORT || 3000;
 
